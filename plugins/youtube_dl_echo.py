6211a63e98c302c3fafd889b565ecd7f52b0be97
jobin888 <64780226+jobin888@users.noreply.github.com>
2021-10-09 21:03:00 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -32,7 +32,7 @@ from helper_funcs.help_uploadbot import DownLoadFile
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 
-@pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*https://t.me/NT_BOT_CHANNEL.*"))
+@pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
@@ -54,7 +54,7 @@ async def echo(bot, update):
             )
             return
         except Exception:
-            await update.reply_text("Join My Updates Channel to use ME")
+            await update.reply_text("Something Wrong. Contact my Support Group")
             return
     logger.info(update.from_user)
     url = update.text
