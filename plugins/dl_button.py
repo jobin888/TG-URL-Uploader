d1cf3e6918c31e5764e31536d7467b38396f5d8a
jobin888 <64780226+jobin888@users.noreply.github.com>
2021-10-15 15:54:44 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -267,7 +267,7 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         await bot.edit_message_text(
             chat_id,
             message_id,
-            text="""Initiating Download
+            text="""**Download**
 URL: {}
 File Size: {}""".format(url, humanbytes(total_length))
         )
@@ -288,11 +288,12 @@ File Size: {}""".format(url, humanbytes(total_length))
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
-                        current_message = """**Download Status**
-URL: {}
-File Size: {}
-Downloaded: {}
-ETA: {}""".format(
+                        current_message = """**Downloading......😜**
+
+**URL:** {}
+**Size:** {}
+**Download:** {}
+**Time:** {}""".format(
     url,
     humanbytes(total_length),
     humanbytes(downloaded),
