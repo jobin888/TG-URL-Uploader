d0b988a2599562069217cdc8d04cac67769b2831
NT BOTS <64780226+jobin888@users.noreply.github.com>
2021-10-19 17:56:34 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -34,21 +34,21 @@ async def help_user(bot, update):
     # logger.info(update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.HELP_MSG,
+        text=Translation.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["plan"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["me"]))
 async def get_me_info(bot, update):
     # logger.info(update)
     chat_id = str(update.from_user.id)
     chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.CURENT_PLAN_MESSAGE.format(chat_id, plan_type, expires_at),
+        text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
@@ -60,7 +60,7 @@ async def start(bot, update):
     # logger.info(update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_MSG,
+        text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
     )
 
@@ -70,7 +70,7 @@ async def upgrade(bot, update):
     # logger.info(update)
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.UPGRADE_MSG,
+        text=Translation.UPGRADE_TEXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
